c52de205ce6f3253fbac1e26496c15cbc9da2c1c
zhuolil <cp8482617>
2016-11-22 23:27:21 -0800

    db configuration done, now could connect to AWS Mysql

---
+++ grails-app/domain/earningmanagementwebserver/Person.groovy
@@ -0,0 +1,17 @@
+package earningmanagementwebserver
+
+class Person {
+
+    static constraints = {
+    }
+
+    String firstName
+    String lastName
+    Date dateOfBirth
+    double salary
+}
+
+class Course {
+    String name
+    String time
+}
