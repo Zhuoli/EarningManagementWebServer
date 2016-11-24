e69d62b8b4736d999459498cc57a762829029520
zhuolil <cp8482617>
2016-11-23 16:05:57 -0800

    Add redirect

--- grails-app/controllers/earningmanagementwebserver/HelloWorldController.groovy
+++ grails-app/controllers/earningmanagementwebserver/HelloWorldController.groovy
@@ -5,9 +5,18 @@ import java.time.LocalDate
 class HelloWorldController {
 
     def index() {
+        redirect(action: "secondAction")
+    }
+
+    def current() {
         StockItems stockItems1 = new StockItems(symbol: 'JD', shares: 50, reportDate: LocalDate.now(), sharedAverageCost: 12.5)
         stockItems1.save()
         def share = StockItems.findBySymbolLike('JD');
 
-        [ share:share ]}
+        [ share:share ]
+    }
+
+    def secondAction() {
+        render "You were redirected from firstAction to secondAction"
+    }
 }
